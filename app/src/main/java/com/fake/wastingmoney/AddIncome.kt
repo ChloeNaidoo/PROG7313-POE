3a686a168c0fbe95ea05ee4ec7a87ae9cb0fe81a
lab_services_student <ST10145067@vcconnect.edu.za>
2025-06-09 16:32:04 +0200

    code attribution

--- app/src/main/java/com/fake/wastingmoney/AddIncome.kt
+++ app/src/main/java/com/fake/wastingmoney/AddIncome.kt
@@ -1,5 +1,29 @@
 package com.fake.wastingmoney
 
+<<<<<<< HEAD
+=======
+/*
+ // Code Attribution:
+ // Author: IIE
+ // Source: Programming 3C Module Manual [PM1]
+ // Student numbers: ST10145067, ST10081245, ST10264009, ST10368647, ST10397162
+
+ // Code Attribution:
+ // Author: IIE
+ // Source: GitHub source code repository for the module [PM2]
+ // URL: https://github.com/iie-PROG7313
+ // [Accessed 25 September 2024]
+ // Student numbers: ST10145067, ST10081245, ST10264009, ST10368647, ST10397162
+
+ // Code Attribution:
+ // Author: JetBrains
+ // Source: Kotlin Documentation [PM3]
+ // URL: https://kotlinlang.org/docs/home.html
+ // [Accessed 25 September 2024]
+ // Student numbers: ST10145067, ST10081245, ST10264009, ST10368647, ST10397162
+ */
+
+>>>>>>> 6a51a25 (code attribution)
 import android.app.DatePickerDialog
 import android.content.Intent
 import android.content.pm.PackageManager
--- app/src/main/java/com/fake/wastingmoney/model/User.kt
+++ app/src/main/java/com/fake/wastingmoney/model/User.kt
@@ -1,4 +1,27 @@
 package com.fake.wastingmoney.model
+<<<<<<< HEAD
+=======
+/*
+ // Code Attribution:
+ // Author: IIE
+ // Source: Programming 3C Module Manual [PM1]
+ // Student numbers: ST10145067, ST10081245, ST10264009, ST10368647, ST10397162
+
+ // Code Attribution:
+ // Author: IIE
+ // Source: GitHub source code repository for the module [PM2]
+ // URL: https://github.com/iie-PROG7313
+ // [Accessed 25 September 2024]
+ // Student numbers: ST10145067, ST10081245, ST10264009, ST10368647, ST10397162
+
+ // Code Attribution:
+ // Author: JetBrains
+ // Source: Kotlin Documentation [PM3]
+ // URL: https://kotlinlang.org/docs/home.html
+ // [Accessed 25 September 2024]
+ // Student numbers: ST10145067, ST10081245, ST10264009, ST10368647, ST10397162
+ */
+>>>>>>> 6a51a25 (code attribution)
 
 data class User(
     val id: Long = 0,
